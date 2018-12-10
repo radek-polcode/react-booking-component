548a188d0781cf20fe8e916f6c173fab704dd6c5
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2018-12-10 11:49:25 +0100

    Style hotel-expectation

--- src/components/RoomInfo.jsx
+++ src/components/RoomInfo.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import './RoomInfo.css'
+import { IconContext } from "react-icons";
+import { FaLaugh } from "react-icons/fa";
 
 export const RoomInfo = () =>
   <div className="col-sm-9">
-    <p>95% of guest reviewers had their expectations of this property met or exceeded</p>
+    <div>
+      <p className="hotel-expectation">
+        <IconContext.Provider value={
+          { 
+            size: "1.5em",
+            className: 'hotel-expectation-icon'
+          }
+        }>
+          <FaLaugh />
+        </IconContext.Provider>
+        95% of guest reviewers had their expectations of this property met or exceeded
+      </p>
+    </div>
     <p>Bunk Bed Room and Shared Bathroom </p>
     <p>Only 2 room left </p>
   </div>
