6b2a1ce966e51db39b7d1a406022610beb372758
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2018-12-10 08:45:58 +0100

    Css, new components, change structure

--- src/components/ItemContent.jsx
+++ src/components/ItemContent.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import MainInfo from './MainInfo';
 import RoomDetails from './RoomDetails';
+
 export const ItemContent = () => 
   <div className="col-sm-8">
     <MainInfo />
--- src/components/MainBlock.jsx
+++ src/components/MainBlock.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import './MainBlock.css';
 
-export const MainBlock = () => 
+export const MainBlock = () =>
   <div className="col-sm-9">
-    <h2>Barcelona Central Garden</h2>
-    <p> Eixample, Barcelona – Show on map (650 m from center ) – Subway Access</p>
+    <a href="https://www.booking.com/hotel/es/barcelona-central-garden-hostel.html?aid=376363&sid=b25d83dd6413a7363d53abd7385acd94">
+      <span className="hotel-name">
+        Barcelona Central Garden
+      </span>
+    </a>
+    <p>
+      <a href="#"
+         className="tool-tip location-info"
+      > 
+        Eixample, Barcelona – Show on map 
+      </a>
+      <span className="additional-location-info tool-tip"> 
+        (650 m from center ) – 
+      </span>
+      <span className="additional-location-info tool-tip">
+        Subway Access
+      </span>
+    </p>
+
   </div>
 
 export default MainBlock;
