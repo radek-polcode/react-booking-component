b06094dc50e5a28b0a8710eaeebbc6a3d7de8492
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2018-12-10 13:33:52 +0100

    Style prices

--- src/components/RoomPrice.jsx
+++ src/components/RoomPrice.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
+import './RoomPrice.css';
 
 export const RoomPrice = () => 
   <div className="col-sm-3">
-    <p>Price for 2 nights</p>
-    <p>608 zł</p>
-    <p>+12,33 zł taxes and charges</p>
+    <p className="room-price-for-x-nights">Price for 2 nights</p>
+    <p className="room-price">608 zł</p>
+    <p className="room-price-taxes-and-fees clear-float">+12,33 zł taxes and charges</p>
     <button>See availability</button>
   </div>
 
