9ba9ce6b5d02d7d150dc53d71fb5875d9af57d0e
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2018-12-11 15:53:43 +0100

    Add data.json

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,8 +2,16 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 import Item from '../components/Item'
+import json_data from './data.js';
 
 class App extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      json_data: JSON.parse(json_data)
+    }
+  }
+
   render() {
     return (
       <div className="container">
---
+++ src/containers/data.js
@@ -0,0 +1,53 @@
+const jsonData = {
+  "data": {
+    "item_1": {
+    "name": "Barcelona Central Garden",
+    "location": "Eixample, Barcelona",
+    "from_center": "650",
+    "subway_access": "true",
+    "expectations_exceeded": "95%",
+    "rooms": {
+      "rooms_name": "Bunk Bed Room and Shared Bathroom",
+      "rooms_left": "Only 2 room left"
+    },
+    "reviews": {
+      "title": "Wonderful",
+      "number_of_reviews": "542",
+      "location_score": "9.5",
+      "guest_favourite": "true"
+    },
+    "price": {
+      "number_of_nights": "2",
+      "price": "608",
+      "taxes_and_charges": "12,33",
+      "currency": "zł"
+    },
+    "thumbnail":"../images/thumb.jpg"
+    },
+    "item_2": {
+      "name": "Apartament Nadwiślańska",
+      "location": "Podgórze, Kraków",
+      "from_center": "2,1km",
+      "subway_access": "false",
+      "expectations_exceeded": "100%",
+      "rooms": {
+        "rooms_name": "Apartment 40m2 - Bigger than most in Kraków",
+        "rooms_left": "Only 1 left on our site"
+      },
+      "reviews": {
+        "title": "Wonderful",
+        "number_of_reviews": "55",
+        "location_score": "9.8",
+        "guest_favourite": "true"
+      },
+      "price": {
+        "number_of_nights": "2",
+        "price": "632",
+        "taxes_and_charges": "60",
+        "currency": "zł"
+      }
+    }
+  }
+}
+
+export default jsonData;
