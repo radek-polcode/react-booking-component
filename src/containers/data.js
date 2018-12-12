886ba0870fa01167ca4646a1a540ba90560db5c0
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2018-12-12 12:42:46 +0100

    Update data

--- src/containers/data.js
+++ src/containers/data.js
@@ -1,35 +1,43 @@
 const jsonData = {
   "data": {
     "item_1": {
-    "name": "Barcelona Central Garden",
-    "location": "Eixample, Barcelona",
-    "from_center": "650",
-    "subway_access": "true",
-    "expectations_exceeded": "95%",
-    "rooms": {
-      "rooms_name": "Bunk Bed Room and Shared Bathroom",
-      "rooms_left": "Only 2 room left"
-    },
-    "reviews": {
-      "title": "Wonderful",
-      "number_of_reviews": "542",
-      "location_score": "9.5",
-      "guest_favourite": "true"
-    },
-    "price": {
-      "number_of_nights": "2",
-      "price": "608",
-      "taxes_and_charges": "12,33",
-      "currency": "zł"
-    },
-    "thumbnail":"/images/thumb.jpg"
+      "main_info": {
+        "name": "Barcelona Central Garden",
+        "location": "Eixample, Barcelona",
+        "from_center": "650",
+        "subway_access": "true",
+        "expectations_exceeded": "95%"
+      },
+      "rooms": {
+        "rooms_name": "Bunk Bed Room and Shared Bathroom",
+        "rooms_left": "Only 2 room left"
+      },
+      "reviews": {
+        "title": "Wonderful",
+        "number_of_reviews": "542",
+        "location_score": "9.5",
+        "guest_favourite": "true"
+      },
+      "price": {
+        "number_of_nights": "2",
+        "price": "608",
+        "taxes_and_charges": "12,33",
+        "currency": "zł"
+      },
+      "thumbnail":"/images/thumb.jpg",
+      "urls": {
+        "main_url": "https://www.booking.com/hotel/es/barcelona-central-garden-hostel.html?aid=376363&sid=b25d83dd6413a7363d53abd7385acd94",
+        "map_url": "https://www.booking.com/hotel/es/barcelona-central-garden-hostel.html?aid=376363&sid=b25d83dd6413a7363d53abd7385acd94&srpvid=0dc7373df6e000ef&checkin=2019-04-29&checkout=2019-05-01&ucfs=1&srpvid=0dc7373df6e000ef&srepoch=1544428283&highlighted_blocks=55438906_88502282_0_0_0&all_sr_blocks=55438906_88502282_0_0_0&room1=A,A&hpos=1&hapos=1&dest_type=city&dest_id=-372490&srfid=57179b06ca2743ec3b9dff0897fe8094ff62afe9X1&from=searchresults;map=1&msd=1#hotelTmpl"
+      }
     },
     "item_2": {
-      "name": "Apartament Nadwiślańska",
-      "location": "Podgórze, Kraków",
-      "from_center": "2,1km",
-      "subway_access": "false",
-      "expectations_exceeded": "100%",
+      "main_info": {
+        "name": "Apartament Nadwiślańska",
+        "location": "Podgórze, Kraków",
+        "from_center": "2,1km",
+        "subway_access": "false",
+        "expectations_exceeded": "100%",
+      },
       "rooms": {
         "rooms_name": "Apartment 40m2 - Bigger than most in Kraków",
         "rooms_left": "Only 1 left on our site"
@@ -46,7 +54,11 @@ const jsonData = {
         "taxes_and_charges": "60",
         "currency": "zł"
       },
-      "thumbnail":"/images/thumb2.webp"
+      "thumbnail":"/images/thumb2.webp",
+      "urls": {
+        "main_url": "",
+        "map_url": ""
+      }
     }
   }
 }
