949f8e0a62f6e56e2f921c9cfca6ca705797a4f9
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2018-12-12 12:06:52 +0100

    Refactor ItemThumbnail functional component

--- src/components/ItemThumbnail.jsx
+++ src/components/ItemThumbnail.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 
+// Separate local imports from dependencies
 import './styles/ItemThumbnail.css';
 
-export const ItemThumbnail = ({thumbnail}) =>
-  <div className="col-4 col-item-thumbnail">
-    <img className='thumbnail'
-        src={thumbnail}
-        alt="thumbnail" />
-  </div>
+// Declare propTypes here, before the component (taking advantage of JS function hoisting)
+// You want these to be as visible as possible
+ItemThumbnail.propTypes = {
+  thumbnail: PropTypes.string
+}
+
+// Avoid the following ES6 syntax:
+// export const ItemThumbnail = ({thumbnail}) => {}
+// Looks very modern, but the function here is actually unnamed.
+
+function ItemThumbnail({ thumbnail }) {
+  return (
+    <div className="col-4 col-item-thumbnail">
+      <img className='thumbnail'
+          src={thumbnail}
+          alt="thumbnail" />
+    </div>
+  )
+}
 
 export default ItemThumbnail;
