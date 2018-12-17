17d4e4efdfa3a4a0fdb670d0772cf4105445a9f7
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2018-12-17 11:36:44 +0100

    Updates

---
+++ precache-manifest.173ff89274aed11f11e3b6e57c4453ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4652d39fb5b3fc5c8ca1",
+    "url": "/react-booking-component/static/css/main.b1f313c0.chunk.css"
+  },
+  {
+    "revision": "4652d39fb5b3fc5c8ca1",
+    "url": "/react-booking-component/static/js/main.4652d39f.chunk.js"
+  },
+  {
+    "revision": "b5d997085595f230924b",
+    "url": "/react-booking-component/static/css/1.59a5b2ca.chunk.css"
+  },
+  {
+    "revision": "b5d997085595f230924b",
+    "url": "/react-booking-component/static/js/1.b5d99708.chunk.js"
+  },
+  {
+    "revision": "53708e407327fff6af21",
+    "url": "/react-booking-component/static/js/runtime~main.53708e40.js"
+  },
+  {
+    "revision": "70317d75e058344e630c1761df499850",
+    "url": "/react-booking-component/index.html"
+  }
+];
